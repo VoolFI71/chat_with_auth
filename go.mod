2dda2b2ccd6ea04eaf8e6b9aa8c131818da9c892
Gleb <gleb.gleb.tula@mail.ru>
2025-02-01 19:24:49 +0300

    .

--- go.mod
+++ go.mod
@@ -14,13 +14,27 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.24.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jackc/pgx/v4 v4.18.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.13.0 // indirect
@@ -30,4 +44,6 @@ require (
 	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/protobuf v1.36.4 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/sqlite v1.5.7 // indirect
+	gorm.io/gorm v1.25.12 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,91 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+	"fmt"
+    "net/http"
+    "encoding/json"
+    "database/sql"
+    _ "github.com/jackc/pgx/v4/stdlib"
+    "log"
 )
 
+type Subj struct {
+    Product string `json:"name"`
+    Price   int    `json:"price"`
+ }
+ 
+func JSONHandler(w http.ResponseWriter, req *http.Request) {
+    // собираем данные
+    subj := Subj{"Milk", 50}
+    // кодируем в JSON
+    resp, err := json.Marshal(subj)
+    if err != nil {
+        http.Error(w, err.Error(), 500)
+        return
+    }
+    // устанавливаем заголовок Content-Type
+    // для передачи клиенту информации, кодированной в JSON
+    w.Header().Set("content-type", "application/json")
+    // устанавливаем код 200
+    w.WriteHeader(http.StatusOK)
+    // пишем тело ответа
+    w.Write(resp)
+} 
+
+func mainPage(res http.ResponseWriter, req *http.Request) {
+    
+    body := fmt.Sprintf("Method: %s\r\n", req.Method)
+    body += "Header ===============\r\n"
+    for k, v := range req.Header {
+        body += fmt.Sprintf("%s: %v\r\n", k, v)
+    }
+    body += "Query parameters ===============\r\n"
+
+    err := req.ParseForm()
+    if err != nil {
+        http.Error(res, "Unable to parse form", http.StatusBadRequest)
+        return
+    }
+
+    for k, v := range req.Form {
+        body += fmt.Sprintf("%s: %v\r\n", k, v)
+    }
+    res.Write([]byte(body))
+} 
+
 func main() {
-    // Создаем новый роутер
-    router := gin.Default()
+    ps := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",`localhost`, `postgres`, `1234`, `go`)
 
-    // Определяем маршрут
-    router.GET("/", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "Hello, World!",
-        })
-    })
+    db, err := sql.Open("pgx", ps)
+    if err != nil {
+        panic(err)
+    }
+    defer db.Close()
 
-    // Запускаем сервер на порту 8080
-    router.Run(":8080")
-}
+    createTableSQL := `
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE
+    );`
+
+    // Выполняем запрос на создание таблицы
+    _, err = db.Exec(createTableSQL)
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    rows, err := db.Query("SELECT * FROM users")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+
+    mux := http.NewServeMux()
+    mux.HandleFunc(`/`, mainPage)
+    mux.HandleFunc(`/a`, JSONHandler)
+    err = http.ListenAndServe(`:8080`, mux)
+    if err != nil {
+        panic(err)
+    }
+} 
