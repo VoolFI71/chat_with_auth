a43b984ede1c28ca6eea5dc8d62ccb56aaea3be6
Gleb <gleb.gleb.tula@mail.ru>
2025-02-17 22:38:11 +0300

    Изображения успешно отправляются по вебсокету но не отображаются при выгрузке из базы даннных

--- backend/internal/websocket/websocket.go
+++ backend/internal/websocket/websocket.go
@@ -22,6 +22,7 @@ type ChatMessage struct {
 	Username  string `json:"username"`
 	Message   string `json:"message"`
 	CreatedAt string `json:"created_at"` 
+	Image     string `json:"image"`
 }
 
 var clients = make(map[*websocket.Conn]bool)
@@ -57,7 +58,6 @@ func SaveMsg(db *sql.DB) gin.HandlerFunc {
     return func(c *gin.Context) {
         var jwtSecret = []byte("123")
 
-        // Извлечение токена из заголовка
         tokenString := c.GetHeader("Authorization")
         if len(tokenString) > 7 && strings.ToLower(tokenString[:7]) == "bearer " {
             tokenString = tokenString[7:]
@@ -68,7 +68,6 @@ func SaveMsg(db *sql.DB) gin.HandlerFunc {
             return
         }
 
-        // Парсинг и проверка токена
         token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
             if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
                 return nil, http.ErrNotSupported
@@ -92,15 +91,12 @@ func SaveMsg(db *sql.DB) gin.HandlerFunc {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Username not found in token claims"})
 			return
 		}
-        // Создание структуры для сообщения
-        //var user ChatMessage
+
 		message := c.PostForm("message")
 
-        // Получение файла изображения
         image, err := c.FormFile("image")
         var imageData []byte
         if err == nil {
-            // Чтение файла изображения в массив байтов
             file, err := image.Open()
             if err != nil {
                 c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to open image"})
@@ -122,10 +118,10 @@ func SaveMsg(db *sql.DB) gin.HandlerFunc {
             return
         }
 
-        // Возвращение успешного ответа
         c.JSON(http.StatusOK, gin.H{"status": "Message saved", "username":  username})
     }
 }
+
 func GetMessagesHandler(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		messages, err := GetLastMessages(db)
@@ -138,7 +134,7 @@ func GetMessagesHandler(db *sql.DB) gin.HandlerFunc {
 }
 
 func GetLastMessages(db *sql.DB) ([]ChatMessage, error) {
-	rows, err := db.Query("SELECT username, message, created_at FROM chat ORDER BY created_at DESC LIMIT 75")
+	rows, err := db.Query("SELECT username, message, created_at, image FROM chat ORDER BY created_at DESC LIMIT 75")
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
@@ -148,11 +144,14 @@ func GetLastMessages(db *sql.DB) ([]ChatMessage, error) {
 	var messages []ChatMessage
 	for rows.Next() {
 		var msg ChatMessage
-        if err := rows.Scan(&msg.Username, &msg.Message, &msg.CreatedAt); err != nil {
+        if err := rows.Scan(&msg.Username, &msg.Message, &msg.CreatedAt, &msg.Image); err != nil {
 			fmt.Println(err)
 			return nil, err
 		}
+
+
 		messages = append(messages, msg)
+
 	}
 
 	if err := rows.Err(); err != nil {
